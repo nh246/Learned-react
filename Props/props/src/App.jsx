d1cbac039e3eed6d9f13c0235fd214db47154e98
nh246 <nazmul210130@diit.edu.bd>
2023-12-13 00:39:58 +0600

    tailwind and props

---
+++ Props/props/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import Card from './components/Card';
+
+function App() {
+
+
+  return (
+    <>
+  
+    <h1>This is card</h1>
+    <Card channel="nh-rakib" />
+    <br/>
+    <Card/>
+
+
+    </>
+  )
+}
+
+export default App
