eef987d8a54c8ad532cab025c47136939aefded2
nh246 <nazmul210130@diit.edu.bd>
2023-12-15 14:38:18 +0600

    password generator

---
+++ Project-5/Password_Generator/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState,useCallback } from "react"
+
+
+function App() {
+
+  const [length, setlength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setcarAllowed] = useState(false);
+  const [password, setpassword] = useState("");
+
+  const [fun , ]
+
+  return (
+<div
+className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+<h1
+className="text-white text-center my-3"
+>Password Generator</h1>
+
+</div>
+  )
+}
+
+export default App
