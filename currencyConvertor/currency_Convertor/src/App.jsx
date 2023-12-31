89b155b1ef5544a6779a80ca967c4f8f15d0f4e0
nh246 <nazmul210130@diit.edu.bd>
2024-01-01 02:05:00 +0600

    adding code

--- currencyConvertor/currency_Convertor/src/App.jsx
+++ currencyConvertor/currency_Convertor/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>This is just a h1</h1>
+      <h1 
+      className="text-3xl bg-orange-500"
+      >This is just a h1</h1>
     </>
   );
 }
