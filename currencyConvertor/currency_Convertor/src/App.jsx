093b7b4f24c884022be33f76a62ace292d9cf93a
nh246 <nazmul210130@diit.edu.bd>
2024-01-02 00:18:43 +0600

    adding code

--- currencyConvertor/currency_Convertor/src/App.jsx
+++ currencyConvertor/currency_Convertor/src/App.jsx
@@ -1,6 +1,18 @@
 import "./App.css";
+import { useState } from "react";
+import {InputBox} from './Components/InputBox';
+import useCurrencyInfo from './hooks/useCurrencyinfo';
+
 
 function App() {
+  const [amount , setAmount] = useState(0)
+  const [from , setFrom] = useState("use")
+  const [to , setTo]= useState("bdt")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  
+
+
+
   return (
     <>
       <h1 
