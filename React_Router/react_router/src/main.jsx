3b81096392aa76cfe553157c3e64419ac754d0a8
nh246 <nazmul210130@diit.edu.bd>
2024-01-05 18:15:14 +0600

    adding code

--- React_Router/react_router/src/main.jsx
+++ React_Router/react_router/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Router } from 'react-router-dom';
+import { Router, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import 
 
+const router = createBrowserRouter (
+  createRoutesFromElements (
+    <Route path='/' element={<Layout/>} >
 
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
