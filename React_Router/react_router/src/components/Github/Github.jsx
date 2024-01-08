e3ed2d9de8591fd5a196fbe8bed30577f2d34d10
nh246 <nazmul210130@diit.edu.bd>
2024-01-08 22:58:15 +0600

    learned about params

---
+++ React_Router/react_router/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+
+
+function Github() {
+  return (
+    <div
+    className="text-center m-4 bg-gray-700 text-white p-4 text-3xl"
+    >Github followers:</div>
+  )
+}
+
+export default Github
--- React_Router/react_router/src/main.jsx
+++ React_Router/react_router/src/main.jsx
@@ -8,6 +8,7 @@ import Layout from './Layout';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import User from './components/User/User';
 
 // const router = createBrowserRouter([
 //     {
@@ -36,6 +37,7 @@ const router = createBrowserRouter (
      <Route path='' element={<Home/>} />
      <Route path='about' element={<About/>} />
      <Route path='contact' element={<Contact/>} />
+     <Route path='user/:userid' element={<User/>} />
     </Route>
   )
 )
