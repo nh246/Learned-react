2ea4cca4273a77d500a03b7ffd5a3cb850300f85
nh246 <nazmul210130@diit.edu.bd>
2024-01-10 00:15:40 +0600

    react router

--- React_Router/react_router/src/main.jsx
+++ React_Router/react_router/src/main.jsx
@@ -9,6 +9,7 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import User from './components/User/User';
+import Github, { githubInfoLoader } from './components/Github/Github';
 
 // const router = createBrowserRouter([
 //     {
@@ -38,6 +39,9 @@ const router = createBrowserRouter (
      <Route path='about' element={<About/>} />
      <Route path='contact' element={<Contact/>} />
      <Route path='user/:userid' element={<User/>} />
+     <Route
+     loader={githubInfoLoader}
+     path='github' element={<Github/>} />
     </Route>
   )
 )
