618d0a0165a478bbb2bd089f2a6286e4501fb56e
nh246 <nazmul210130@diit.edu.bd>
2024-01-15 22:06:57 +0600

    adding code

---
+++ Theme-switcher/theme_switcher/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1
+        className="text-black-200 p-5 w-2"
+      >
+        This is an heading
+      </h1>
+    </>
+  );
+}
+
+export default App;
