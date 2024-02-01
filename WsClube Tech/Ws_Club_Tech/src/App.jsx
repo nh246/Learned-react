2c64841765015954972f6cdc3bc3c21abbada253
nh246 <nazmul210130@diit.edu.bd>
2024-02-01 23:03:39 +0600

    adding code

--- WsClube Tech/Ws_Club_Tech/src/App.jsx
+++ WsClube Tech/Ws_Club_Tech/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner'
 import Header from './components/Header'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Banner/>
     </>
   )
 }
---
+++ WsClube Tech/Ws_Club_Tech/src/components/Banner.jsx
@@ -0,0 +1,18 @@
+
+function Banner() {
+  return (
+    <div className="bg-[#2699fb] w-full py-[100px] " >
+
+        <div className="max-w-[1240px] mx-auto text-center font-bold">
+            <div className="text-white">
+                Learn with us 
+            </div>
+            <h2>Grow with us.</h2>
+            <div>Learn </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner
