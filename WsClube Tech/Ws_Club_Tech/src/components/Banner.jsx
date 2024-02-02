faa24aa49bccb8af9a3cd040b20c5dfd9cfe2a05
nh246 <nazmul210130@diit.edu.bd>
2024-02-02 14:30:50 +0600

    adding code

--- WsClube Tech/Ws_Club_Tech/src/components/Banner.jsx
+++ WsClube Tech/Ws_Club_Tech/src/components/Banner.jsx
@@ -1,14 +1,14 @@
 
 function Banner() {
   return (
-    <div className="bg-[#2699fb] w-full py-[100px] " >
+    <div className="bg-[#2699fb] w-full md:py-[100px] py-[35px] " >
 
         <div className="max-w-[1240px] mx-auto text-center font-bold">
-            <div className="text-white">
+            <div className="md:text-3xl text-2xl">
                 Learn with us 
             </div>
-            <h2>Grow with us.</h2>
-            <div>Learn </div>
+            <h2 className="text-white md:text-5xl text-3xl md:py-2 md:mt-3 mt-2">Grow with us.</h2>
+            <div className="text-white md:text-2xl md:py-3 mt-2 text-1xl">Learn </div>
         </div>
 
     </div>
