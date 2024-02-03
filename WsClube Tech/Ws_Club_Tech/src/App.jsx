c3df34d906cd80ea66a3692fcdd5bea78f143967
nh246 <nazmul210130@diit.edu.bd>
2024-02-03 21:13:58 +0600

    adding code

--- WsClube Tech/Ws_Club_Tech/src/App.jsx
+++ WsClube Tech/Ws_Club_Tech/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Banner from './components/Banner'
 import Experts from './components/Experts'
 import Header from './components/Header'
+import NewsLetter from './components/NewsLetter'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Banner/>
       <Experts/>
+      <NewsLetter/>
     </>
   )
 }
