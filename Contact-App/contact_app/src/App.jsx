161a075d7eec5136487de02eea285928367883f7
nh246 <nazmul210130@diit.edu.bd>
2024-02-18 22:12:54 +0600

    adding code

--- Contact-App/contact_app/src/App.jsx
+++ Contact-App/contact_app/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { LuSearch } from "react-icons/lu";
+import { LuSearch } from 'react-icons/lu';
+import { AiFillPlusCircle } from "react-icons/ai";
 
 function App() {
   return (
-    <div className='max-w-[370px] mx-auto'> 
-      <Navbar/>
-      {/* search bar  */}
+    <div className="max-w-[370px] mx-auto px-4">
+      <Navbar />
       <div className='flex'>
-      <LuSearch className='text-white text-2xl ' />
-        <input 
-        type='text'
-        className='h-10 flex-grow rounded-md bg-transparent border-white border-2 '
-        />
+        <div className="flex relative items-center">
+          <LuSearch className="text-white text-3xl flex p-1 absolute" />
+          <input
+            type="text"
+            className="h-10 flex-grow pl-7 pr-1 rounded-lg bg-transparent border-white border-2 text-white text-1xl "
+          />
+        </div>
+        <AiFillPlusCircle />
       </div>
+      {/* search bar  */}
     </div>
   );
 }
