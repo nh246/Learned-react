7613280efffdbfd023e1b0d53aab4aa262be5355
nh246 <nazmul210130@diit.edu.bd>
2024-03-27 01:06:12 +0600

    adding auth to blogg

--- Mega-blog/mega_blog/src/appwrite/auth.js
+++ Mega-blog/mega_blog/src/appwrite/auth.js
@@ -12,13 +12,14 @@ export class Authservice {
         this.account = new Account(this.clint);
     
     }
-
+// the create account 
     async createAccount({email,password,name}){
         try {
          const userAccount = await this.account.create(ID.unique(), email,password,name);
 
          if (userAccount) {
             // call another method 
+            return this.login({email, password});
 
          } else {
             return userAccount;
@@ -27,6 +28,37 @@ export class Authservice {
             throw error;
         }
     }
+// the user login 
+    async login({email,password}) {
+        try {
+           return await this.account.createEmailSession(email,password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+// get current user 
+
+async getCurrentUser(){
+    try {
+        return await this.account.get()
+    }
+    catch (error){
+        console.log("This is an appwirte error :: getCurrentUser::error",error);
+    }
+
+    return null;
+}
+// logout sessions
+ 
+async logout(){
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        console.log("This is an appwrite logout:: error",error);
+    }
+}
+
 
 }
 
