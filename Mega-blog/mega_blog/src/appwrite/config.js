dd41f9c37dddbfe3ecf19fe43279a4899607d634
nh246 <nazmul210130@diit.edu.bd>
2024-03-28 16:19:02 +0600

    configeration

--- Mega-blog/mega_blog/src/appwrite/config.js
+++ Mega-blog/mega_blog/src/appwrite/config.js
@@ -1,5 +1,6 @@
 import conf from "../conf/conf";
 import { Client,ID , Databases,Storage,Query } from "appwrite";
+import User from './../../../../React_Router/react_router/src/components/User/User';
 
 export class Service{
     client = new Client();
@@ -15,8 +16,130 @@ export class Service{
 
     }
     
-    async 
+    // Creating the post in databases
 
+    async createPost ({title, slug, content, freaturedImage, status, userID}) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    freaturedImage,
+                    status,
+                    userID,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite create post" , error);
+        }
+    }
+// updating the post in the databases
+
+async updatePost (slug,{title, content, freaturedImage, status}){
+
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                freaturedImage,
+                status
+            }
+        )
+    } catch (error) {
+        console.log("appwrite updatePost ",error);
+    }
+}
+
+// deleting the post in database
+
+async deletePost(slug){
+    try {
+        await this.databases.deleteDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+        return true;
+    } catch (error) {
+        console.log("Appwrite deleting post",error);
+        return false;
+    }
+}
+
+// getting a post 
+
+async getPost(slug){
+    try {
+        return await this.databases.getDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug
+        )
+    } catch (error) {
+        console.log("appwrite getting post ", error);
+        
+    }
+}
+
+async getPosts (queries = [Query.equal("status","active")]){
+  try {
+     return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries,
+
+     )
+  } catch (error) {
+    console.log("Appwrite getPosts",error);
+    return false
+  }
+}
+
+// file upload services
+
+async uploadFile (file){
+ try {
+    return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+    )
+ } catch (error) {
+    console.log("Appwrite file doenload",error);
+    
+ }
+}
+
+// delete file services
+
+async deleteFile(fileId){
+    try {
+        await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId
+        )
+        return true
+    } catch (error) {
+        console.log("Appwrite delete file",error);
+        
+    }
+}
+
+// file preview
+
+getFilePreview(fileId){
+    return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId
+    )
+}
 
 
 }
