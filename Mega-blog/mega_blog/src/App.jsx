77faa87fdc383003f9f258ac0d30f4fc38235823
nh246 <nazmul210130@diit.edu.bd>
2024-04-13 17:46:33 +0600

    adding

--- Mega-blog/mega_blog/src/App.jsx
+++ Mega-blog/mega_blog/src/App.jsx
@@ -1,10 +1,29 @@
 
-
+import {useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+   
+  useEffect (()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } 
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally()
+  }
+  ,[])
+
+
   return (
     <>
 <h1>Hallow world</h1>
