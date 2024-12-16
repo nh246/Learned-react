6811790430c4afdb6e730c83f3a3a18ef06ebd69
nh246 <nazmul210130@diit.edu.bd>
2024-12-16 16:32:40 +0600

    adding code

---
+++ Fire-Auth/fire_auth/src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgdfXZrK4HNH_b29Nm3yNSPts7f0hum2g",
+    authDomain: "ema-jhon-fd853.firebaseapp.com",
+    projectId: "ema-jhon-fd853",
+    storageBucket: "ema-jhon-fd853.firebasestorage.app",
+    messagingSenderId: "181338148340",
+    appId: "1:181338148340:web:a8f640a964565d12847da1"
+  };
+
+  export default firebaseConfig;
